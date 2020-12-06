70eb198bc082b7990c9815a2ba32f3cd91992e91
david-shakaya <devidoz666@gmail.com>
2020-12-06 22:09:11 +0200

    Основной функционал готов

--- src/index.js
+++ src/index.js
@@ -1,1 +1,44 @@
 import '../src/styles.css'
+import menuListItems from './menu.json'
+import menuItems from '../templates/menu-items.hbs'
+
+// Дом узлы
+const ulMenuRef = document.querySelector('.js-menu');
+const inputThemeSwitchRef = document.querySelector('.theme-switch__toggle');
+const bodyRef = document.querySelector('body');
+
+// Добавление разметки
+const markup = menuItems(menuListItems)
+ulMenuRef.insertAdjacentHTML('beforeend', markup)
+
+// Cлушатели
+inputThemeSwitchRef.addEventListener('change', changeTheme)
+
+
+
+const tagListItem = document.querySelectorAll('.tag-list__item');
+tagListItem.forEach(el => {
+    if (el.textContent === '') {
+        el.remove()
+    }
+})
+
+
+function changeTheme(e) {
+    if (e.target.checked) {
+        // запысывае в локал тема черная
+        localStorage.setItem('theme', 'dark');
+        if (localStorage.getItem('theme') === 'dark') {
+            bodyRef.classList.add('dark-theme')
+        }
+        }
+    else {
+        localStorage.removeItem('theme');
+        bodyRef.classList.remove('dark-theme')
+    }
+    
+}
+if (localStorage.getItem('theme')) {
+    bodyRef.classList.add('dark-theme')
+    inputThemeSwitchRef.checked = true
+}
